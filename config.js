4717e5e279cc20933d32888756602bbaa808561d
Jeflacc <109415855+Jeflacc@users.noreply.github.com>
2026-03-19 23:12:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 // You can change your API URL here whenever you restart your Cloudflare tunnel.
 const CONFIG = {
-    API_URL: "https://lan-louis-teeth-shelter.trycloudflare.com"
+    API_URL: "https://lets-timeline-define-anyway.trycloudflare.com"
 };
