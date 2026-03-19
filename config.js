cabfba6d1ead7e2447ed65e6afbe3121af1e0ece
Jeflacc <109415855+Jeflacc@users.noreply.github.com>
2026-03-19 14:44:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 // You can change your API URL here whenever you restart your Cloudflare tunnel.
 const CONFIG = {
-    API_URL: "https://criterion-implied-rainbow-connected.trycloudflare.com"
+    API_URL: "https://lan-louis-teeth-shelter.trycloudflare.com"
 };
