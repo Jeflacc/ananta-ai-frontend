1a77ca40620b0c607097dca4aafa429b448fa73e
Jeflacc <109415855+Jeflacc@users.noreply.github.com>
2026-03-20 01:01:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 // You can change your API URL here whenever you restart your Cloudflare tunnel.
 const CONFIG = {
-    API_URL: "https://playlist-century-technologies-coding.trycloudflare.com"
+    API_URL: "https://strain-signature-fleet-consultation.trycloudflare.com"
 };
